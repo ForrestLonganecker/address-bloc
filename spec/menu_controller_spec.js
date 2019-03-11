c1d088d7cef1c471dae8ed824f5ce2ee6b7c8997
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-11 14:28:48 -0700

    Testing assignment work completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -10,11 +10,18 @@ describe("MenuController", () => {
 
 		it("should return 0 when no contacts are in the book", () => {
 			expect(this.menu.getContactCount()).toBe(0);
-		});
+		})
 
 		it("should return 1 when there is exactly one contact in the book", () => {
 			this.menu.contacts.push("Bob");
-			expect(this.menu.getContactCount()).toBe(1)
-		});
+			expect(this.menu.getContactCount()).toBe(1);
+		})
+	});
+
+	describe("#remindMe()", () => {
+
+		it("should return 'Learning is a life-long pursuit' when called", () => {
+			expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+		})
 	});
 });
