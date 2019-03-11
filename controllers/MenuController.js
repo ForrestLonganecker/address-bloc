67cb4297401762247202a46674e7cae7c713083c
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-11 14:21:53 -0700

    Testing checkpoint material complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -49,4 +49,8 @@ module.exports = class MenuController {
         console.log("Thanks for using AddressBloc!");
         process.exit();
     }
+
+    getContactCount(){
+        return this.contacts.length;
+    }
 };
