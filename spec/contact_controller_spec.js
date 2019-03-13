ebc78268e3de11c0f69fce75f96a87570b686075
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-13 11:21:07 -0700

    assignment material completed

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -20,10 +20,11 @@ describe("ContactController", () => {
 
     describe("#addContact()", () => {
         it("should add a single contact into the book", (done) => {
-            this.book.addContact("Alice", "001-101-1010")
+            this.book.addContact("Alice", "001-101-1010", "alice@mail.com")
             .then((contact) => {
                 expect(contact.name).toBe("Alice");
                 expect(contact.phone).toBe("001-101-1010");
+                expect(contact.email).toBe("alice@mail.com");
                 done();
             })
             .catch((err) => {
