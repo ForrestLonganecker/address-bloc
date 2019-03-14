ec0d91d88674f4107c6842ee2fa2323c9801f6bd
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-03-14 08:42:49 -0700

    Listing and searching checkpoint completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,6 +10,8 @@ module.exports = class MenuController {
                 message: "Please choose from an option below: ",
                 choices: [
                     "Add new contact",
+                    "View all contacts",
+                    "Search for a contact",
                     "Exit"
                 ]
             }
@@ -26,6 +28,12 @@ module.exports = class MenuController {
                     break;
                 case "Exit":
                     this.exit();
+                case "View all contacts":
+                    this.getContacts();
+                    break;
+                case "Search for a contact":
+                    this.search();
+                    break;
                 default:
                     console.log("Invalid input");
                     this.main();
@@ -42,7 +50,7 @@ module.exports = class MenuController {
 
     addContact(){
         this.clear();
-        inquirer.prompt(this.book.addContacQuestions).then((answers) => {
+        inquirer.prompt(this.book.addContactQuestions).then((answers) => {
             this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
                 console.log("Contact added successfully!");
                 this.main();
@@ -52,9 +60,96 @@ module.exports = class MenuController {
             });
         });
     }
+
+    getContacts(){
+        this.clear();
+        this.book.getContacts().then((contacts) => {
+            for(let contact of contacts) {
+                console.log(`
+                    name: ${contact.name}
+                    phone number: ${contact.phone}
+                    email: ${contact.email}
+                    --------------------`
+                );            
+            }
+            this.main();
+        }).catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
     
     exit(){
         console.log("Thanks for using AddressBloc!");
         process.exit();
+    }
+
+    search(){
+        inquirer.prompt(this.book.searchQuestions)
+        .then((target) => {
+            this.book.search(target.name)
+            .then((contact) => {
+                if(contact === null){
+                    this.clear();
+                    console.log("contact not found");
+                    this.search();
+                } else {
+                    this.showContact(contact);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    showContact(contact){
+        this._printContact(contact);
+        inquirer.prompt(this.book.showContactQuestions)
+        .then((answer) => {
+            switch(answer.selected){
+                case "Delete contact":
+                    this.delete(contact);
+                    break;
+                case "Main menu":
+                    this.main();
+                    break;
+                default:
+                    console.log("Something went wrong.");
+                    this.showContact(contact);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            this.showContact(contact);
+        });
+    }
+
+    _printContact(contact){
+        console.log(`
+            name: ${contact.name}
+            phone number: ${contact.phone}
+            email: ${contact.email}
+            --------------------`
+        );
+    }
+
+    delete(contact){
+        inquirer.prompt(this.book.deleteConfirmQuestions)
+        .then((answer) => {
+            if(answer.confirmation) {
+                this.book.delete(contact.id);
+                console.log("contact deleted!");
+                this.main();
+            } else {
+                console.log("contact not deleted");
+                this.showContact(contact);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            this.main();
+        });
     }
 };
